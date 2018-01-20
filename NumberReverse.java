691a40d9648dd6730c8fffadc867a97b336362eb
macverick6912 <umang.srivastava.ckc@gmail.com>
2018-01-20 18:29:57 +0530

    Program for reverse of a number.

---
+++ NumberReverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import  java.lang.*;
+class NumberReverse{
+	public static void main(String a[]){
+		Scanner scanner= new Scanner(System.in);
+		int n,c,d,rev=0,i=0;
+		n=sc.nextInt();
+		while (n!=0) {
+			d=n%10;
+			rev=rev+d*(Math.pow(10,i));
+			n=n/10;
+			i++;
+		}
+		System.out.print(rev);
+	}
+}
